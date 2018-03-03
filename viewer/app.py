8fb35d3339cb42c809b4df1bf151bfcd3b82bec6
johnchase <chasejohnh@gmail.com>
2018-03-02 17:49:51 -0800

    Updated alignment

--- viewer/app.py
+++ viewer/app.py
@@ -10,26 +10,18 @@ import io
 import datetime
 import plotly.figure_factory as ff
 import plotly.graph_objs as go
-
+from collections import OrderedDict
+import json
 
 msa = skbio.alignment.TabularMSA.read('/Users/jc33/Dropbox/plotly/msa10.fna', constructor=DNA)
 base_dic = {'A': 1, 'C': .25, 'G': .5, 'T': .75, '-': 0}
-
-def seq_align_for_plot(msa):
-    base_text = [list(str(e)) for e in msa]
-    base_values = np.zeros((len(base_text), len(base_text[0])))
-    for i in range(len(base_text[0])):
-        for j in range(len(base_text)):
-            if base_text[j][i] != base_text[0][i]:
-                base_values[j][i] = base_dic[base_text[j][i]]
-    return(base_text, base_values)
 
 def get_tick_vals(start, stop):
     tick_vals = np.arange(start, stop, 10)
     tick_vals = np.array([9, 19, 29]) - (tick_vals%10)
     return tick_vals
 
-base_text, base_values = seq_align_for_plot(msa)
+
 names = [seq.metadata['id'] for seq in msa]
 colorscale_blocks = [[0.00, '#F4F0E4'],
               [0.25, '#1b9e77'],
@@ -57,10 +49,11 @@ app.layout = html.Div(children=[
             ),
 
     html.Div([
+    html.Label('Reference Sequence'),
     dcc.Dropdown(
         id='parent-seq',
         options=[{'label': label, 'value': label} for label in names],
-        value=names[0])], style={'width': '25%', 'display': 'inline-block'}),
+        value=names[0])], style={'width': '22%', 'display': 'inline-block'}),
 
     dcc.Graph(id='alignment',
         config={
@@ -70,22 +63,41 @@ app.layout = html.Div(children=[
         id='alignment-slider',
         updatemode='drag',
         min=0,
-        max=len(base_values[0]) - 30,
+        max=len(msa[0]) - 30,
         value=0,
         step=1
         )
-    ], style={'width': '80%', 'display': 'inline-block'})
+    ], style={'width': '80%', 'display': 'inline-block'}),
+    html.Div(id='ordered-values', style={'display': 'none'})
     ]
 )
+
+
+@app.callback(
+        dash.dependencies.Output('ordered-values', 'children'), 
+        [dash.dependencies.Input('parent-seq', 'value')])
+def seq_align_for_plot(name):
+    seq_dic = OrderedDict(zip(names, msa))
+    seq_dic.move_to_end(name, last=False)
+    base_text = [list(str(seq_dic[e])) for e in seq_dic]
+    base_values = np.zeros((len(base_text), len(base_text[0])))
+    for i in range(len(base_text[0])):
+        for j in range(len(base_text)):
+            if base_text[j][i] != base_text[0][i]:
+                base_values[j][i] = base_dic[base_text[j][i]]
+    updated_names = list(seq_dic.keys())
+    jdump = json.dumps([base_text, base_values.tolist(), updated_names])
+    return(jdump)
+
 
 @app.callback(
     dash.dependencies.Output('alignment', 'figure'),
     [dash.dependencies.Input('alignment-slider', 'value'),
-     dash.dependencies.Input('layout-type', 'value')])
-def update_figure(start, layout):
+     dash.dependencies.Input('layout-type', 'value'),
+     dash.dependencies.Input('ordered-values', 'children')])
+def update_figure(start, layout, bases):
     stop = start + 30
     tick_values = get_tick_vals(start, stop)
-    print(layout) 
     if layout == 'Blocks':
         colorscale = colorscale_blocks
         font_properties = {
@@ -99,12 +111,12 @@ def update_figure(start, layout):
                 'size':16,
                 'color': '#000'
                 }
-
+    base_text, base_values, ordered_names = json.loads(bases)
     base_subset = np.array(base_values)[:, start:stop]
     text_subset = np.array(base_text)[:, start:stop]
     
     fig = ff.create_annotated_heatmap(base_subset,
-                                      y=names,
+                                      y=ordered_names,
                                       annotation_text=text_subset,
                                       colorscale=colorscale)
     
@@ -123,7 +135,7 @@ def update_figure(start, layout):
             yaxis=dict(autorange='reversed',
                ticks='',
                ticksuffix='  ',
-               ticktext=names,
+               ticktext=ordered_names,
                tickvals=list(np.arange(0, len(base_text))),
                showticklabels=True),
             
@@ -141,7 +153,7 @@ def update_figure(start, layout):
     if layout == 'Letters':
         for letter in fig.layout.annotations:
             letter['font']['color'] = letter_colors[letter['text']]
-            if letter['text'] != '-' and letter['y'] != '1AM1JR7QWMSFA_8958':
+            if letter['text'] != '-' and letter['y'] != ordered_names[0]:
                 if letter['text'] == text_subset[0][letter['x']]:
                     letter['text'] = '.'
     return fig
